e290eeae9adc9d92be59a64900ae56e078f03001
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-23 21:07:18 +0200

    The initial commit.

---
+++ src/main/java/bm/app/githubuserrepostars/GithubUserRepoStarsApplication.java
@@ -0,0 +1,13 @@
+package bm.app.githubuserrepostars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserRepoStarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUserRepoStarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bm/app/githubuserrepostars/GithubUserRepoStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.githubuserrepostars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserRepoStarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
